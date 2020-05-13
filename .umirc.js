788dacc0c36382aeca7a66918fc5f00d7a02c9eb
lencx <cxin1314@gmail.com>
2020-05-13 11:35:39 +0800

    chore: hd

--- .umirc.js
+++ .umirc.js
@@ -1,3 +1,4 @@
+import px2rem from 'postcss-plugin-px2rem';
 
 // ref: https://umijs.org/config/
 export default {
@@ -23,6 +24,18 @@ export default {
       dynamicImport: { webpackChunkName: true },
       title: '盎司一起',
       dll: true,
+      hd: {
+        theme: {
+          // antd-mobile 高清方案
+          '@hd': '2px',
+        },
+        // more: https://github.com/pigcan/postcss-plugin-px2rem#configuration
+        px2rem: {
+          rootValue: 50,
+          minPixelValue: 2,
+          selectorBlackList: ['ex_'], //以包含ex_的class不需要转换
+        },
+      },
 
       pwa: {
         manifestOptions: {
--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import cns from 'classnames';
 import 'zarm/dist/zarm.min.css';
 
-import './index.less';
+import styles from './index.less';
 
 export default function Layout({ className, hasNav, children, ...rest }) {
   return (
-    <div className={cns('z-layout', className)}>
+    <div className={cns(styles.zLayout, className)}>
       <div className='z-container'>{children}</div>
       {hasNav && <div className='z-navbar'></div>}
     </div>
