8f03033121cf7a3d79cb2131fb7e50e888905232
吴人杰 <wurenjie@zhongan.io>
2020-05-18 18:27:47 +0800

    hotrecommend and cardItem

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import { connect } from 'dva';
 import Portal from '@/components/Portal';
-
+import HotRecommend from '@/components/HotRecommend'
 import styles from './style/index.less';
+import sample from '@/assets/sample.png';
+import ActivityCard from '@/components/ActivityCard'
+const imgList = [
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+  {
+    thumbnail:sample,
+    image:sample,
+    link:'www.baidu.com'
+  },
+]
 
 function Home ({ dispatch }) {
   // const clickHandler = () => {
@@ -11,11 +45,16 @@ function Home ({ dispatch }) {
   //     payload: '123123323',
   //   });
   // }
+  const data = {
+    percent:0.3
+  }
   return (
     <div className={styles.homePage}>
       <Portal node="#wx_head .header_before">
         <span onClick={() => console.log('click')}>{'back'}</span>
       </Portal>
+      <ActivityCard data={data}  />
+      <HotRecommend list={imgList}/>
       {new Array(50).fill(1).map((i, idx) => <p key={idx}>盎司一起测试滚动区域</p>)}
     </div>
   );
