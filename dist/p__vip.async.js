be7f01cedb722d21ff706195d8a61747840d5842
chenzaining <chenzaining@zhongan.io>
2020-05-18 19:24:43 +0800

    update dist

---
+++ dist/p__vip.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[7],{"0Eg9":function(n,e,a){"use strict";a.r(e);var t=a("q1tI"),c=a.n(t),i=a("fDDD"),s=a.n(i);e["default"]=(()=>{return c.a.createElement("div",{className:s.a.vipPage},"VIP PAGE")})},fDDD:function(n,e,a){}}]);
