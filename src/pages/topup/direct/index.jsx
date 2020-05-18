3ca971c3eb71208a251d3495914b6b13ba264061
lencx <cxin1314@gmail.com>
2020-05-18 16:13:29 +0800

    chore: spec

---
+++ src/pages/topup/direct/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SpecGroup from '@/components/SpecGroup';
+
+export default function DirectPage() {
+  return (
+    <div>
+      <SpecGroup />
+    </div>
+  );
+}
