1de7199e587cdf5831c9852cb365bd5e69e63c3c
lencx <cxin1314@gmail.com>
2020-05-20 14:37:34 +0800

    chore: card

--- src/pages/explore/index.jsx
+++ src/pages/explore/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Card from '@/components/Card';
 
 import styles from './index.less';
 
 function ExplorePage() {
   return (
     <div className={styles.explorePage}>
-      EXPLORE PAGE
+      <Card title="商品列表" extra={<span>测试</span>}>
+        Product List
+      </Card>
     </div>
   );
 }
