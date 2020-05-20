916e3d6ab6fda75721b43da10e6545c578d2c0e9
lencx <cxin1314@gmail.com>
2020-05-20 11:37:52 +0800

    chore: router

---
+++ src/pages/explore/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './index.less';
+
+function ExplorePage() {
+  return (
+    <div className={styles.explorePage}>
+      EXPLORE PAGE
+    </div>
+  );
+}
+
+export default ExplorePage;
