7e1f1f1fd69846e85c679bf1b55030e273e62bb9
xufengyu <1171264915@qq.com>
2020-05-21 16:42:13 +0800

    successbuy

--- .umirc.js
+++ .umirc.js
@@ -25,6 +25,8 @@ export default {
         { path: '/orderdetail', component: '../pages/orderdetail', backgroundColor:'gray',title: '订单详情' },
         { path: '/phone', component: '../pages/phone', backgroundColor:'gray',title: '话费充值', hasBuyFooter: true },
         { path: '/spike', component: '../pages/spike', backgroundColor:'gray',title: '秒杀活动',  },
+        { path: '/successbuy',component: '../pages/successbuy', title: '购买成功',  },
+
       
         // { path: '/test', component: '../pages/test' },
       ],
