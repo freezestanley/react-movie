cb7d7b1ddb964efe1c3a0c89fd0b302432b4cda6
lencx <cxin1314@gmail.com>
2020-05-22 11:21:58 +0800

    build

---
+++ dist/precache-manifest.99d3b27f1d69198bf69296b1141d1d68.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = [
+  {
+    "revision": "13021f176cff231d82af",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "13021f176cff231d82af",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "50d983c52533d81f9bbe",
+    "url": "/layouts__common.chunk.css"
+  },
+  {
+    "revision": "50d983c52533d81f9bbe",
+    "url": "/layouts__common.async.js"
+  },
+  {
+    "revision": "6f63a310a6171cc0f3a1",
+    "url": "/p__cardpackage.chunk.css"
+  },
+  {
+    "revision": "6f63a310a6171cc0f3a1",
+    "url": "/p__cardpackage.async.js"
+  },
+  {
+    "revision": "8190d58b8e4e3b373b72",
+    "url": "/p__charge.chunk.css"
+  },
+  {
+    "revision": "8190d58b8e4e3b373b72",
+    "url": "/p__charge.async.js"
+  },
+  {
+    "revision": "5f35eff72025f3bf46f1",
+    "url": "/p__explore.chunk.css"
+  },
+  {
+    "revision": "5f35eff72025f3bf46f1",
+    "url": "/p__explore.async.js"
+  },
+  {
+    "revision": "da0bb6074483d5f93cad",
+    "url": "/p__home.chunk.css"
+  },
+  {
+    "revision": "da0bb6074483d5f93cad",
+    "url": "/p__home.async.js"
+  },
+  {
+    "revision": "a3640ccfd5c750664af6",
+    "url": "/p__home__models__home.js.async.js"
+  },
+  {
+    "revision": "dfb1178b8d0ed9ea110f",
+    "url": "/p__login.chunk.css"
+  },
+  {
+    "revision": "dfb1178b8d0ed9ea110f",
+    "url": "/p__login.async.js"
+  },
+  {
+    "revision": "ee7e111b022f637f7190",
+    "url": "/p__my.chunk.css"
+  },
+  {
+    "revision": "ee7e111b022f637f7190",
+    "url": "/p__my.async.js"
+  },
+  {
+    "revision": "b90f0e1ee8339342b940",
+    "url": "/p__orderdetail.chunk.css"
+  },
+  {
+    "revision": "b90f0e1ee8339342b940",
+    "url": "/p__orderdetail.async.js"
+  },
+  {
+    "revision": "dfd34fefc9f6352936f9",
+    "url": "/p__orders.chunk.css"
+  },
+  {
+    "revision": "dfd34fefc9f6352936f9",
+    "url": "/p__orders.async.js"
+  },
+  {
+    "revision": "860df45a182b18da04f5",
+    "url": "/p__phone.chunk.css"
+  },
+  {
+    "revision": "860df45a182b18da04f5",
+    "url": "/p__phone.async.js"
+  },
+  {
+    "revision": "7f81e44e2ecf539148e3",
+    "url": "/p__seckill.chunk.css"
+  },
+  {
+    "revision": "7f81e44e2ecf539148e3",
+    "url": "/p__seckill.async.js"
+  },
+  {
+    "revision": "765e65dbbe12ec18c50e",
+    "url": "/p__successbuy.chunk.css"
+  },
+  {
+    "revision": "765e65dbbe12ec18c50e",
+    "url": "/p__successbuy.async.js"
+  },
+  {
+    "revision": "5f44d317ceafb2c2cf4c",
+    "url": "/p__topup.chunk.css"
+  },
+  {
+    "revision": "5f44d317ceafb2c2cf4c",
+    "url": "/p__topup.async.js"
+  },
+  {
+    "revision": "f1c1212b202076d63b94",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "f1c1212b202076d63b94",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "94c681b6cd3f2813c9f31133e332fec5",
+    "url": "/static/footer-bg-gray.94c681b6.png"
+  },
+  {
+    "revision": "7f6c00824264be32aa562a7a73f2ce6c",
+    "url": "/static/vip-bg.7f6c0082.png"
+  },
+  {
+    "revision": "f524f01769ebfca685f50fca521a6727",
+    "url": "/static/sample.f524f017.png"
+  },
+  {
+    "revision": "c8432287f6d3714cadd9e8a55c0bcf7e",
+    "url": "/static/footer-bg.c8432287.png"
+  },
+  {
+    "revision": "27c3a9c3ae7a2dfefb4c4995bdb014a5",
+    "url": "/static/me-bg.27c3a9c3.png"
+  },
+  {
+    "revision": "7043481c14eb5784f7508e90462cfb06",
+    "url": "/static/tab-bg.7043481c.png"
+  },
+  {
+    "revision": "81d5ed6971a2d4a2efe69d95fb299f7b",
+    "url": "/static/product-bg.81d5ed69.png"
+  },
+  {
+    "revision": "ee57ed0eceea48bff9bbc96b98b4d81e",
+    "url": "/static/logo.ee57ed0e.png"
+  },
+  {
+    "revision": "91d774be5b0b727f977e4e2a6f29de9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f6a5307a950c19ae9c7b3740c14d1c",
+    "url": "/icons/128.png"
+  },
+  {
+    "revision": "ec2c01423c80d74465f63c3da5fe389b",
+    "url": "/icons/144.png"
+  },
+  {
+    "revision": "b8bfa99aa7d0662f27da3f26ba7318f3",
+    "url": "/.DS_Store"
+  },
+  {
+    "revision": "4a93a04c8af0922475875657d511efa2",
+    "url": "/icons/192.png"
+  },
+  {
+    "revision": "bd4990cb16d90b5b3d7bf579d01f4f28",
+    "url": "/icons/256.png"
+  },
+  {
+    "revision": "191074b6f2222c2440b0bb3a0c642364",
+    "url": "/icons/512.png"
+  },
+  {
+    "revision": "8b1c8020cc67ebf844a1615ad394999a",
+    "url": "/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0efa5a654f225a5a603396a7f7aa8518",
+    "url": "/icons/android-chrome-152x152.png"
+  },
+  {
+    "revision": "d7e488ade6dbf67eef4668ddd15c5b1d",
+    "url": "/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "7f791a77d6f2e2a30b570c2aa31fb7fa",
+    "url": "/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f3b51acf7ce636366ba888475144b429",
+    "url": "/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "98e177e60162bc26af57a5fdd65ab69e",
+    "url": "/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "719cb777b3c5dd6f5f2288e538c98df1",
+    "url": "/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "bbedf478fa6adc4b1c8faa146a262bd2",
+    "url": "/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "47cb723696508f2c5b8e5f704a31220f",
+    "url": "/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "18f17462b535693be93bc1184774c5c9",
+    "url": "/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "8abb1cc4c5b8dc8db59e3026d135eb1d",
+    "url": "/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "b92ee177ec93f530d294b6ac137a95b2",
+    "url": "/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "17ce9d5e4c6f674e8654255db087e432",
+    "url": "/MP_verify_rKD1zGVtaanIQ8NY.txt"
+  },
+  {
+    "revision": "9cc732a216a64fa60ccd6e6800643a75",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "af2204ea0c88aaab35511768985c8020",
+    "url": "/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  }
+];
