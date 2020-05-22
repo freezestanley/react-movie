6edc56232c88cf4e4b75b4b3614c7f3a314a2edb
chenzaining <chenzaining@zhongan.io>
2020-05-22 16:59:05 +0800

    支付产品信息～

--- src/models/prePay.js
+++ src/models/prePay.js
@@ -1,9 +1,10 @@
+// 底部支付Bar需要展示的信息&需要传给支付按钮的信息
 export default {
   namespace: 'prePay',
   state: {
-    main: {},
-    attach: {},
-    type: 'product'  // 4种类型: product|seckill|vip|promote|phone
+    main: {},  // 主产品信息
+    attach: {}, // 附加产品信息
+    type: 'product'  // 支付产品类型，共4种类型: product|seckill|vip|promote|phone
   },
   reducers: {
     setState(state, { payload }) {
