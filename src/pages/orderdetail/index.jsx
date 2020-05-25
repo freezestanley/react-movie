044d8e33bd9fb66d312607594df0a133876c7878
xufengyu <1171264915@qq.com>
2020-05-25 15:12:32 +0800

    all

--- src/pages/orderdetail/index.jsx
+++ src/pages/orderdetail/index.jsx
@@ -2,30 +2,22 @@ import React from 'react';
 import styles from './index.less'
 import CardCharge from '@/components/CardCharge'
 import OrderDetails from '@/components/OrderDetails';
-import AllBuy from './../../components/RecommendBuy'
+import AllBuy from '@/components/RecommendBuy'
 
 export default()=>{
   return(
-
     <div className={styles.myorder}>
-
       <div className={styles.pay}>
         <span className={styles.span_1}>待支付</span>
         <span  className={styles.span_2}>1000元</span>
-
       </div>
       <div className={styles.paytime}>
         <div>04 : 58</div>
         <div>等待付款，秒杀折扣将为您保留至倒计时结束</div>
-
       </div>
-     
-
-     
       <OrderDetails/>
       <CardCharge/>
       <AllBuy/>
-
     </div>
   )
 }
