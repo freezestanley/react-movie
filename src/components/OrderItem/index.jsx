61605deb00ac6eccc149a7f8937e3086177eeb98
xufengyu <1171264915@qq.com>
2020-05-26 14:14:53 +0800

    orderlist

--- src/components/OrderItem/index.jsx
+++ src/components/OrderItem/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './index.less';
 export default ( {onClick})=>{
+  const mock={
+    paystate:3//1待支付 2 充值失败 3 已支付 4 失效
+  }
   return(
     <div className={styles.order} onClick={onClick}>
       <div className={styles.order_up}>
         <div className={styles.order_head}>
           <img  alt=''/>
           <span className={styles.span_1}>腾讯视屏会员+蜜蜂会员卡白金</span>
-          <span className={styles.span_2}>待支付</span>
+          <div className={styles.pay}>
+            {mock.paystate==1&&(<span className={styles.unpaid}>待支付 </span>)} 
+            {mock.paystate==2&&(<span className={styles.chagrefailed}>充值失败</span>)}
+            {mock.paystate==3&&(<span className={styles.paid}>已支付</span>)}
+            {mock.paystate==4&&(<span className={styles.outtime}>已失效</span> )}
+            </div>
         </div>
         <p className={styles.up_p}>¥ 22</p>
         <div className={styles.details}>
@@ -16,11 +24,13 @@ export default ( {onClick})=>{
           <p className={styles.details_p1}>规格 : 月卡 </p>
         </div>
       </div>
-      <div className={styles.order_down}> 
+      {mock.paystate!=4&&(<div className={styles.order_down}> 
         <div>
-           <span>支付</span>
+          {mock.paystate==1&&( <span className={styles.recharge}>支付</span> )}
+          {mock.paystate==2&&(<span className={styles.concat}>联系客服</span>)}
+          {mock.paystate==3&&(<span className={styles.check}>查看卡密</span>)}
         </div>
-      </div>
+      </div>)}
   </div>
   )
 }
