35270f134eb93a85db6014a8a046d15895bc6feb
huangshaohui <huangshaohui@zhongan.io>
2020-05-28 17:41:32 +0800

    发现页面开发

--- src/pages/explore/Categories/index.jsx
+++ src/pages/explore/Categories/index.jsx
@@ -1,18 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import classNames from 'classnames/bind';
 import styles from './index.module.less';
 
 const cx = classNames.bind(styles);
 
 export default ({ selected, list, onSelect }) => {
+  const curIndex = useMemo(() => {
+    return list.findIndex(item => item.id === selected);
+  }, [list, selected]);
+
   return (
     <div className={styles['cates']}>
-      {list.map(({ id, name }) => {
+      {list.map(({ id, name }, idx) => {
         return (
           <div
             key={id}
             className={cx('cate', {
               selected: id === selected,
+              prev: curIndex > -1 && idx === curIndex - 1,
+              next: curIndex > -1 && idx === curIndex + 1,
             })}
             onClick={() => {
               onSelect && onSelect(id);
--- src/pages/explore/index.jsx
+++ src/pages/explore/index.jsx
@@ -78,7 +78,10 @@ export default connect(state => ({ explore: state.explore }))(({ dispatch, explo
     <div className={styles['page']}>
       <div className={styles['search']}></div>
       <div className={styles['cates']}>
-        <Categories selected={selected} list={categories2use} onSelect={onCateSelected} />
+        <div className={styles['cates-inner']}>
+          <Categories selected={selected} list={categories2use} onSelect={onCateSelected} />
+          <div className={styles['cates-placeholder']}></div>
+        </div>
       </div>
       {categories2use.map(({ id, name, products }) => {
         return (
