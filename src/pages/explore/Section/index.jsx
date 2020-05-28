e765d2c2333301ec091f7c00f7ffd57b1aeeef6b
huangshaohui <huangshaohui@zhongan.io>
2020-05-28 20:52:39 +0800

    发现页面开发

--- src/pages/explore/Section/index.jsx
+++ src/pages/explore/Section/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './index.module.less';
 
-export default ({ id, title, children }) => {
+export default ({ title, children }) => {
   return (
-    <div id={id} className={styles['section']}>
+    <div className={styles['section']}>
       <div className={styles['title']}>{title}</div>
       <div>{children}</div>
     </div>
