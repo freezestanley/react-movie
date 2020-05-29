e89eb7d6f6caeff81e2729085e884cc3a386ab3d
chenyou <2716698534@qq.com>
2020-05-29 16:06:25 +0800

    package ui change

--- src/pages/card/active/month.jsx
+++ src/pages/card/active/month.jsx
@@ -6,12 +6,15 @@ import CardPopup from '../components/CardPopup'
 import {address, store} from '../address'
 import Item from '../components/item'
 import {createPackageOrder} from './util'
-import top from '../img/month/month_01.png'
-import month from '../img/month/month_02.png'
+import month1 from '../img/month/month_01.png'
+import month2 from '../img/month/month_02.png'
+import month3 from '../img/month/month_03.png'
+import month4 from '../img/month/month_04.png'
+import month5 from '../img/month/month_05.png'
+import month6 from '../img/month/month_06.png'
+import month7 from '../img/month/month_07.png'
 import foot from '../img/month/month.png'
-import author from '../img/month/month_03.png'
-import card1 from '../img/month/month_04.png'
-import card2 from '../img/month/month_05.png'
+
 import styles from '../style/active.less';
 
 const Active = ({
@@ -61,9 +64,21 @@ const Active = ({
     }
     return (
         <div ref={box} className = {styles.Active} onScroll = {throttle(scrollClick, 30)}>
-            <img src={top} />
-            <img src={month} />
-            {
+            <img src={month1} />
+            <img src={month2} />
+            <img src={month3} />
+            <img src={month4} />
+            <div className={styles.storelist} onClick={viewStoreList}>查看泰康拜博口腔门店列表</div>
+            <CardPopup 
+                visible = {visible}
+                CloseClick = {viewStoreList}
+                title = {'泰康拜博门店列表'}
+            >
+                <Item address={address} store={store}/>
+            </CardPopup>
+            <img src={month5} />
+            <img src={month6} />
+            {/* {
                 isWX ? (<img src={card1} />) : (
                     <div>
                         <img src={author} />
@@ -80,9 +95,10 @@ const Active = ({
             }
             {
                 isWX ? null : (<img src={card2} />)
-            }
+            } */}
+            <img src={month7} />
             <div className = {styles.info}>
-                <div className = {styles.title}>活动规则</div>
+                {/* <div className = {styles.title}>活动规则</div> */}
                 <div className = {styles.txt}>
                     每个月1号可以开始使用，当月不用视为过期作废
                     下单后3个商品将发放至订单详情内领取
