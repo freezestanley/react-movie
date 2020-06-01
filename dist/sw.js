cf1b77580d5279ca25c592c424dbaf07dfb00b5b
chenzaining <chenzaining@zhongan.io>
2020-06-01 16:14:37 +0800

    update dist

--- dist/sw.js
+++ dist/sw.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.6e01ff1ccef8707deb8bfde589940a98.js"
+  "/precache-manifest.6d45017c4c2e5bf6608d18f29b750141.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ants-app"});
