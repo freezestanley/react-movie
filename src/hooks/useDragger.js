7a362ea890aae7ac48d7bbd0bea6febc16e2ca9a
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-06-16 17:26:09 +0800

    xx

--- src/hooks/useDragger.js
+++ src/hooks/useDragger.js
@@ -58,8 +58,8 @@ const useDragger = (target,
                 let m_distance = Math.floor(Math.sqrt(Math.pow((re_pageX - pageX ), 2) + Math.pow((re_pageY - pageY ), 2)))
                 let result = (Math.floor(m_distance) / distance) * re_size
                 result = result >= 1.85 ? 1.85 : result <= .8 ? .8 : result
-                target.current.style.setProperty('--scale', `${result}`);
-                re_size = result
+                target.current.style.setProperty('--scale', `${Math.floor(result*100)/100}`);
+                re_size = Math.floor(result*100)/100
         } else {
             currX = Math.floor(parseInt(target.current.style.getPropertyValue('--transformX'))) || 0;
             currY = Math.floor(parseInt(target.current.style.getPropertyValue('--transformY'))) || 0;
