19f4221a588388c3c5c0f984f2db4ffbcae91567
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-06-16 19:49:34 +0800

    xx

--- src/hooks/useZoom.js
+++ src/hooks/useZoom.js
@@ -50,7 +50,7 @@ const useZoom = (target,
                 // re_pageY = 10
                 m_distance = Math.floor(Math.sqrt(Math.pow((re_pageX - pageX ), 2) + Math.pow((re_pageY - pageY ), 2)))
                 result = (m_distance / distance) * re_size
-                result = result >= 1.85 ? 1.85 : result <= .8 ? .8 : result
+                // result = result >= 1.85 ? 1.85 : result <= .8 ? .8 : result
                 // re_size = Math.floor(result*100)/100
         } 
         moveEvent(e, {size: re_size, s: `${m_distance} || ${distance} || ${result} || ${re_size}`})
@@ -58,7 +58,7 @@ const useZoom = (target,
     let touchEndHandler = function (e) {
         // e.preventDefault()
         if (e.touches[1]) {
-            re_size = target.current.style.getPropertyValue('--scale')
+            // re_size = target.current.style.getPropertyValue('--scale')
             re_size = re_size >= 1.8 ? 1.8 : re_size <= .7 ? .7 : re_size
             
         } 
