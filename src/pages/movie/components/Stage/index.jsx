168bfae3a93726c7c01c57c834636571f8282f86
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-06-16 17:16:36 +0800

    xx

--- src/pages/movie/components/Stage/index.jsx
+++ src/pages/movie/components/Stage/index.jsx
@@ -210,6 +210,7 @@ const Stage = (props) => {
 
     return (
             <div className={styles.stage} ref={stageRef}>
+                <div>{size}</div>
                 <Detail />
                 <div className={styles.stageBox}>
                     <Preview data={sitFilter} choose={state} show={isTouch} />
