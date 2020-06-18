53c680d5ffaf337db6fa1526db77c103aade8d81
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-06-18 18:00:26 +0800

    xx

--- src/pages/movie/index.jsx
+++ src/pages/movie/index.jsx
@@ -13,30 +13,30 @@ const site = row => {
             {id:`${n}-${n+1}`,state:1,info:`${n}排1座`},
             {id:`${n}-${n+2}`,state:1,info:`${n}排1座`},
             {id:`${n}-${n+3}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+4}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+5}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+6}`,state:5,info:`${n}排1座`},
-            // {id:`${n}-${n+7}`,state:5,info:`${n}排1座`},
-            // {id:`${n}-${n+8}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+9}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+10}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+11}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+12}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+13}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+14}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+15}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+16}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+17}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+18}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+19}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+20}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+21}`,state:4,info:`${n}排1座`},
-            // {id:`${n}-${n+22}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+23}`,state:7,info:`${n}排1座`},
-            // {id:`${n}-${n+24}`,state:7,info:`${n}排1座`},
-            // {id:`${n}-${n+25}`,state:1,info:`${n}排1座`},
-            // {id:`${n}-${n+26}`,state:2,info:`${n}排1座`},
-            // {id:`${n}-${n+27}`,state:2,info:`${n}排1座`},
+            {id:`${n}-${n+4}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+5}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+6}`,state:5,info:`${n}排1座`},
+            {id:`${n}-${n+7}`,state:5,info:`${n}排1座`},
+            {id:`${n}-${n+8}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+9}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+10}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+11}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+12}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+13}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+14}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+15}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+16}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+17}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+18}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+19}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+20}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+21}`,state:4,info:`${n}排1座`},
+            {id:`${n}-${n+22}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+23}`,state:7,info:`${n}排1座`},
+            {id:`${n}-${n+24}`,state:7,info:`${n}排1座`},
+            {id:`${n}-${n+25}`,state:1,info:`${n}排1座`},
+            {id:`${n}-${n+26}`,state:2,info:`${n}排1座`},
+            {id:`${n}-${n+27}`,state:2,info:`${n}排1座`},
         ]
     })
 }
@@ -50,14 +50,14 @@ const Movie = (props) => {
         // result = e.slice()
     },[])
 
-    const data = site(5);
+    const data = site(50);
     // const data = site
 
     
 
     return (
         <div className={styles.movie}>
-            <div className={styles.title}>3D MAX 最新电影33</div>
+            <div className={styles.title}>3D MAX 最新电影44</div>
             <div className={styles.movieDetail}>
                 <div>2019-03-06(周五)</div>
                 <div>16:50</div>
