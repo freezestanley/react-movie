64915b7cf5cd9e315184dd7310c5ea40893feea5
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-06-22 16:32:58 +0800

    add ts

--- public/a.ts
+++ public/a.ts
@@ -1,1 +1,6 @@
 console.log('welcome to deno')
+const listener = Deno.listen({ port: 8080 });
+console.log("listening on 0.0.0.0:8080");
+for await (const conn of listener) {
+  Deno.copy(conn, conn);
+}
